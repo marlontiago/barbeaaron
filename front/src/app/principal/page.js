dd695c26a3cfee898b0b2c6fe13cfbd5e3dd0467
marlonprado <marlontiago@gmail.com>
2025-07-22 23:00:01 -0300

    Criado a section serviços e feito melhorias na section principal

--- front/src/app/principal/page.js
+++ front/src/app/principal/page.js
@@ -1,9 +1,43 @@
+import { Raleway, Roboto } from "next/font/google"
+import Link from 'next/link';
+
+const raleway = Raleway({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-raleway",
+    display: "swap",
+})
+
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-roboto",
+    display: "swap",
+})
+
 export default function Principal(){
 
     return(
         <div>
-            <h1 className="text-3xl font-bold mt-20">Bem-vindo à nossa página!</h1>
-            <p className="mt-4">Explore nossos serviços e saiba mais sobre nós.</p>
+            <h1 className={`text-6xl font-bold mt-20 p-10 ${raleway.className}`}>Bem-vindo à BarbeAaron, onde o seu estilo é levado a sério!</h1>
+            <div className="flex">
+            <p className={` w-200 p-8 text-xl text-justify indent-8 leading-8 ${roboto.className}`}>Aqui, cada cliente é único. Por isso, oferecemos um atendimento totalmente personalizado: você não é apenas mais um corte na cadeira. Nossa equipe é formada por profissionais especializados que entendem do assunto e sabem transformar uma simples visita em uma experiência completa de cuidado e autoestima.
+
+            Corte, barba ou aquele trato no visual? Tudo é feito do jeito que você gosta, com atenção aos detalhes e aquele bate-papo descontraído que só uma barbearia de verdade oferece.
+
+            Sinta-se em casa. Escolha seu estilo. A gente cuida do resto.
+            <br/>
+            <br/>
+            <Link href="#servicos" className={`${roboto.className} inline-block text-right indent-0  bg-black text-white px-3 py-2 rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-110`}>Conheça nossos serviços</Link>
+            </p>
+            
+            <img src="corte1.jpg" alt="Barbearia" className="w-170 h-100 ml-4 object-cover rounded-lg shadow-lg " />
+
+            
+            </div>
+
+            
+
         </div>
     )
 }
--- front/src/app/servicos/page.js
+++ front/src/app/servicos/page.js
@@ -1,16 +1,54 @@
+import { Dancing_Script, Basic } from "next/font/google"
+
+const dancingScript = Dancing_Script({
+    subsets: ["latin"],
+    weight: ["400", "700"],
+    variable: "--font-dancing-script",
+    display: "swap",
+})
+
+const basic = Basic({
+    subsets: ["latin"],
+    weight: ["400"],
+    variable: "--font-basic",
+    display: "swap",
+})
 
 export default function Servicos(){
     return(
-        <div className="bg-pink-600 w-full h-160 p-8">            
-                <h2 className="text-3xl font-bold text-black mb-4">Nossos Serviços</h2>
-                <p className="text-black mb-4">Oferecemos uma variedade de serviços para atender às suas necessidades de beleza e bem-estar. Confira abaixo alguns dos nossos principais serviços:</p>
-                <ul className="list-disc pl-5 text-black">
-                    <li>Cortes de cabelo</li>
-                    <li>Barba e bigode</li>
-                    <li>Tratamentos capilares</li>
-                    <li>Manicure e pedicure</li>
-                </ul>
+
+        <div><h1 className={`text-4xl text-center bg-amber-500 font-bold text-black p-2 ${basic.className}`}>Nossos Serviços</h1>
+        <div className="bg-amber-500 w-full flex h-160 p-4"> 
             
+                                     
+
+            <div className=" rounded-2xl bg-yellow-500/20 w-1/3 text-center">
+
+                <h2 className={` text-7xl ${dancingScript.className}`}>Cabelo</h2>
+                <p className={`justify text-xl mt-4 ${basic.className}`}>Estilo, precisão e cuidado em cada corte. Do clássico ao moderno, deixamos você com o visual ideal.</p>
+                <img src="corte2.jpg" alt="Corte2" className="rounded-full w-85 h-85 mx-auto mt-8" />
+
+            </div>
+
+            <div className=" w-1/3 rounded-2xl bg-yellow-500/20 m-2 text-center">
+
+                <img src="barba1.jpg" alt="Barba1" className="rounded-full w-85 h-85 mx-auto  mb-6" />
+                <h2 className={`rounded-2xl p-4 text-7xl ${dancingScript.className}`}>Barba</h2>
+                <p className={`justify text-xl  ${basic.className}`}>Barba feita com técnica e atenção aos detalhes. Linha perfeita e acabamento impecável para realçar seu estilo.</p>                
+
+            </div>
+
+            <div className="rounded-2xl bg-yellow-500/20 m-2 w-1/3 text-center">
+                
+                <h2 className={`rounded-2xl text-7xl ${dancingScript.className}`}>Freestyle</h2>
+                <p className={`justify text-xl mt-4 ${basic.className}`}>Designs personalizados com lâmina para quem quer ousar. Arte na cabeça com criatividade e atitude.</p>
+                <img src="cabelo4.jpg" alt="Cabelo4" className="rounded-full w-85 h-85 mx-auto mt-8" />
+
+            </div>
+
+
+
+        </div>
         </div>
     )
 }
